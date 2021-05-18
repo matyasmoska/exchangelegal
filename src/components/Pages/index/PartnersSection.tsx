a3fa918076b9b0969afa391084d9b82ba28df0c7
wingeant <antonin.wingender@cz.ibm.com>
2021-05-18 10:21:31 +0200

    partners

--- src/components/Pages/index/PartnersSection.tsx
+++ src/components/Pages/index/PartnersSection.tsx
@@ -1,21 +1,25 @@
-import React, { FC } from "react";
-import { Fade } from "react-awesome-reveal";
-import { c } from "../../../services/misc";
+import React, { FC } from 'react'
+import { Fade } from 'react-awesome-reveal'
+import { c } from '../../../services/misc'
+import Image from 'next/image'
 
 const PartnersSection: FC = () => {
-    return (
-        <div className={c("w-full space-y-12 mb-96 mt-52 px-36", "md:px-8 md:py-16")}>
-            <h2 className="text-4xl font-bold text-center">Hlavní Partneři</h2>
-            <div className={c("flex justify-center space-x-32", 'md:flex-col md:space-x-0 md:space-y-16')}>
-                <Fade damping={0.5} duration={500} cascade triggerOnce>
-                    <img alt="google-logo" className="w-72" src={'/images/google.png'}  />
-                    <img alt="google-logo" className="w-72" src={'/images/google.png'}  />
-                    <img alt="google-logo" className="w-72" src={'/images/google.png'}  />
-                </Fade>
-            </div>
-        </div>
-
-    );
+	return (
+		<div className={c('w-full space-y-12 mb-96 mt-52 px-36', 'md:px-8 md:py-16')}>
+			<h2 className="text-4xl font-bold text-center">Hlavní Partneři</h2>
+			<div className={c('flex justify-center space-x-32', 'md:flex-col md:space-x-0 md:space-y-16')}>
+				<Fade damping={0.5} duration={500} cascade triggerOnce>
+					<a href="http://www.ondato.com/" target="_blank" rel="noreferrer">
+						<Image height={54} width={204} alt="ondato-logo" className="h-14" src={'/images/ondato.png'} />
+					</a>
+					<a href="https://www.pozemkovekompenzace.cz/" target="_blank" rel="noreferrer">
+						<Image height={54} width={429} alt="pkf-logo" className="h-14" src={'/images/PKF logo.png'} />
+					</a>
+					<img alt="bill-logo" className="h-14" src={'/images/Bill_logo_horizontal.svg'} />
+				</Fade>
+			</div>
+		</div>
+	)
 }
 
-export default PartnersSection;
+export default PartnersSection
--- src/components/Pages/services/ServiceItem.tsx
+++ src/components/Pages/services/ServiceItem.tsx
@@ -1,9 +1,9 @@
-import { AnimatePresence, motion } from 'framer-motion'
+import { AnimatePresence, motion as m } from 'framer-motion'
 import { Dispatch, FC, MouseEventHandler, useMemo } from 'react'
 import { heightAnimation } from '../../../animations/navigation'
 import { c } from '../../../services/misc'
 import Button from '../../Layout/Button'
-import { AnimatedCheckmarkIcon, CheckmarkIcon } from '../../Layout/Icons'
+import { AnimatedCheckmarkIcon, CheckmarkIcon, CloseIcon } from '../../Layout/Icons'
 
 export interface ServiceItemType {
 	name: string
@@ -33,29 +33,39 @@ export const ServiceItem: FC<ServiceItemProps> = ({ serviceItem, selectedItems, 
 			onClick={() => {
 				if (!isSelected) setSelectedItems([ ...selectedItems, serviceItem ])
 			}}
-			className={c("relative flex flex-col justify-between p-8 text-left cursor-pointer text-dark-blue shadow-tile", '3xl:p-6')}
+			className={c(
+				'relative flex flex-col justify-between p-8 text-left cursor-pointer text-dark-blue shadow-tile',
+				'3xl:p-6'
+			)}
 		>
 			<div className="flex flex-col justify-between h-full space-y-4">
-				<img src={serviceItem.icon} className={c("w-12", 'md:self-center')} />
-				<h3 className={c("text-2xl font-bold", '3xl:text-xl', 'md:text-center md:text-2xl')}>{serviceItem.name}</h3>
+				<img src={serviceItem.icon} className={c('w-12', 'md:self-center')} />
+				<h3 className={c('text-2xl font-bold', '3xl:text-xl', 'md:text-center md:text-2xl')}>
+					{serviceItem.name}
+				</h3>
 				<p className={c('text-justify', '3xl:text-sm', 'md:text-justify')}>{serviceItem.description}</p>
 				<div className="flex items-end justify-between">
 					<p className="mb-0.5 text-sm">Cena od</p>
-					<p className={c("text-3xl font-bold text-orange-primary", '3xl:text-2xl')}>{`${serviceItem.price},- Kč`}</p>
+					<p
+						className={c('text-3xl font-bold text-orange-primary', '3xl:text-2xl')}
+					>{`${serviceItem.price},- Kč`}</p>
 				</div>
 			</div>
 			<AnimatePresence>
 				{isSelected && (
-					<motion.div
+					<m.div
 						{...heightAnimation}
-						className={c("absolute top-0 left-0 flex flex-col items-center justify-center w-full h-full space-y-12 overflow-hidden text-white bg-dark-blue bg-opacity-90", '3xl:space-y-8 3xl:px-2')}
+						className={c(
+							'absolute top-0 left-0 flex flex-col items-center justify-center w-full h-full space-y-12 overflow-hidden text-white bg-dark-blue bg-opacity-90',
+							'3xl:space-y-8 3xl:px-2'
+						)}
 					>
+						<m.div onClick={deselectItem}>
+							<CloseIcon className="absolute w-8 h-8 transition transform top-5 right-5 hover:scale-110" />
+						</m.div>
 						<CheckmarkIcon className="w-12 h-12" />
-						<h3 className={c("text-2xl font-bold", '3xl:text-xl')}>{serviceItem.name}</h3>
-						<Button type="basic" className="px-6 py-2 text-sm" onClick={deselectItem}>
-							Odebrat
-						</Button>
-					</motion.div>
+						<h3 className={c('text-2xl font-bold', '3xl:text-xl')}>{serviceItem.name}</h3>
+					</m.div>
 				)}
 			</AnimatePresence>
 		</div>
