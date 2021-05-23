70c24cf3abba750c1b6e3949de2fe565179413d6
wingeant <antonin.wingender@cz.ibm.com>
2021-05-23 15:20:32 +0200

    cookies

---
+++ src/components/Layout/CookieBar.tsx
@@ -0,0 +1,44 @@
+import { motion } from 'framer-motion'
+import Link from 'next/link'
+import React from 'react'
+import { useCookies } from 'react-cookie'
+import { useCookie } from 'react-use'
+import data from '../../data/pages/cookies.json'
+import { c } from '../../services/misc'
+import Button from './Button'
+
+const CookieBar = () => {
+	const [cookies, setCookie, removeCookie] = useCookies(['cookie-consent'])
+
+	return (
+		<motion.div
+			className={c(
+				'fixed bottom-0 flex items-center justify-between w-full py-3 text-white bg-dark-blue bg-opacity-95',
+				'px-36',
+				'2xl:px-24',
+				'xl:px-16',
+                'md:flex-col md:px-4 md:justify-start md:space-y-4'
+			)}
+            initial={{ y: 100 }}
+            animate={{ y: 0 }}
+            exit={{ y: 100, opacity: 0 }}
+            transition={{ type: 'tween', duration: 0.5 }}
+		>
+			<div className="flex flex-col space-y-1">
+				<h3 className="font-bold">{data.header}</h3>
+				<p>{data.subheader} <Link href="/privacypolicy"><a className={c("underline", 'md:block')}>{data.moreInfoText}</a></Link> </p>
+			</div>
+			<Button
+				type="basic"
+				className="px-8 py-2"
+				onClick={() => {
+					setCookie('cookie-consent', true)
+				}}
+			>
+				{data.confirmButtonText}
+			</Button>
+		</motion.div>
+	)
+}
+
+export default CookieBar
--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,22 +1,31 @@
-import Head from 'next/head';
-import React, { FC, useEffect } from "react";
-import Footer from '../components/Layout/Footer';
-import Navigation from '../components/Layout/Navigation';
-import SEO from '../components/Layout/SEO';
+import Head from 'next/head'
+import React, { FC, useEffect } from 'react'
+import Footer from '../components/Layout/Footer'
+import Navigation from '../components/Layout/Navigation'
+import SEO from '../components/Layout/SEO'
+import { useCookie } from 'react-use'
+import CookieBar from '../components/Layout/CookieBar'
+import { AnimatePresence } from 'framer-motion'
+import { useCookies } from 'react-cookie'
 
 const DefaultLayout: FC = ({ children }) => {
-    return (
-        <div className="text-dark-blue">
-            <Head>
-                <title>AML solutions</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <SEO />
-            <Navigation />
-            {children}
-            <Footer />
-        </div>
-    );
+	const [cookies, setCookie, removeCookie] = useCookies(['cookie-consent'])
+
+	useEffect(() => { console.log(cookies['cookie-consent']) }, [ cookies ])
+
+	return (
+		<div className="text-dark-blue">
+			<Head>
+				<title>AML solutions</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<SEO />
+			<Navigation />
+			{children}
+			<Footer />
+			<AnimatePresence>{!cookies['cookie-consent'] && <CookieBar />}</AnimatePresence>
+		</div>
+	)
 }
 
-export default DefaultLayout;
+export default DefaultLayout
