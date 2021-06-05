361b72e084ef3a16deba2f9932460d48efa8608c
AMLsolutionsCZ <78909867+AMLsolutionsCZ@users.noreply.github.com>
2021-06-05 13:15:50 +0200

    Update fail.tsx

--- src/pages/potrebuji-aml/fail.tsx
+++ src/pages/potrebuji-aml/fail.tsx
@@ -13,7 +13,7 @@ const ObligationsFailPage = () => {
 				>
 					<div className="flex flex-col items-center space-y-6 text-center">
 						<div>
-							<img src="/images/thumb-up.svg" className={c("w-16 h-16", 'md:w-20 md:h-20')} />
+							<img src="/images/thumb-up.png" className={c("w-16 h-16", 'md:w-20 md:h-20')} />
 						</div>
 						<h1 className="text-3xl font-bold">AML povinnosti se na Vás pravděpodobně nevztahují!</h1>
 						<p className="px-6 mx-auto text-center">
