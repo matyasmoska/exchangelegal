1f3e6a4e8a86a46878e72433075d49bb837fe04e
AMLsolutionsCZ <78909867+AMLsolutionsCZ@users.noreply.github.com>
2021-06-14 15:14:44 +0200

    Update obchodni-podminky.tsx

--- src/pages/obchodni-podminky.tsx
+++ src/pages/obchodni-podminky.tsx
@@ -1,6 +1,6 @@
 import DefaultLayout from '../layouts/DefaultLayout'
 import { c } from '../services/misc'
-import data from '../data/pages/Obchodní%20podmínky%20AML%20solutions.pdf'
+import data from '../data/pages/Obchodni_podminky_AML_solutions.pdf'
 import React from 'react'
 import ParagraphOrMultiple from '../components/Layout/ParagraphOrMultiple'
 
