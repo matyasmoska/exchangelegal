b8c4c5de2b54ff1aefb82b6403a1aab24003de94
AMLsolutionsCZ <78909867+AMLsolutionsCZ@users.noreply.github.com>
2021-07-05 15:43:47 +0200

    Update system-vnitrnich-zasad.tsx

--- src/pages/system-vnitrnich-zasad.tsx
+++ src/pages/system-vnitrnich-zasad.tsx
@@ -3,13 +3,13 @@ import ParagraphOrMultiple from "../components/Layout/ParagraphOrMultiple";
 import DefaultLayout from "../layouts/DefaultLayout";
 import { c } from "../services/misc";
 import Image from 'next/image'
-import pageData from '../data/pages/obligations/obligations.json'
+import pageData from '../data/pages/system-vnitrnich-zasad/system-vnitrnich-zasad.json'
 import Button from "../components/Layout/Button";
 import Link from "next/link";
 // @ts-ignore
-import TopPartMdx from "../data/pages/obligations/topPart.mdx"
+import TopPartMdx from "../data/pages/system-vnitrnich-zasad/topPart.mdx"
 // @ts-ignore
-import BottomPartMdx from "../data/pages/obligations/bottomPart.mdx"
+import BottomPartMdx from "../data/pages/system-vnitrnich-zasad/bottomPart.mdx"
 
 const ObligationsPage = () => {
     return (
