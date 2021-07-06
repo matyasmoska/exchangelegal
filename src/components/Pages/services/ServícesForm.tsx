300853090cffe7877c6fe6546a4497b1dec156d1
AMLsolutionsCZ <78909867+AMLsolutionsCZ@users.noreply.github.com>
2021-07-06 15:25:04 +0200

    Update ServícesForm.tsx

--- src/components/Pages/services/ServícesForm.tsx
+++ src/components/Pages/services/ServícesForm.tsx
@@ -18,15 +18,19 @@ const ServicesForm: FC<{ form: any; visibleRef: any }> = ({ form, visibleRef }) 
 				'md:max-w-none, md:px-4 md:py-16'
 			)}
 		>
-			<a href="/prijimame-kryptomeny" target="_blank">
+			
 				<img className={c('w-16 h-16', 'md:w-12 md:h-12')} src="/images/services.svg" alt="Services icon" />
-			</a>
+			
 			
 			<h2 className="text-4xl font-bold">Nezávazná poptávka</h2>
 			<p className={c('md:text-sm')}>
 				Vyplněním údajů a kliknutím tlačítka "Nezávazně poptat" nezávazně poptáte nabídku služeb.
 			</p>
+			
+			<a href="/prijimame-kryptomeny" target="_blank">
 			<img src="/images/crypto_blue.svg" className={c('w-1/2 rounded-lg', 'md:w-3/4')} />
+			</a>
+			
 			<SelectedServicesList form={form} />
 			<div ref={visibleRef} className={c('grid w-full grid-cols-2 gap-4', 'md:block md:space-y-6')}>
 				<TextInput
