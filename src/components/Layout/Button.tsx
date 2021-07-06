8a1cb32f68d854c90fbaaef6743f7cbd0919d17d
AMLsolutionsCZ <78909867+AMLsolutionsCZ@users.noreply.github.com>
2021-07-06 10:17:45 +0200

    Update Button.tsx

--- src/components/Layout/Button.tsx
+++ src/components/Layout/Button.tsx
@@ -29,3 +29,26 @@ const Button: FC<ButtonProps> = ({ type, className, onClick, disabled = false, c
 }
 
 export default Button
+	
+	
+const Button2: FC<ButtonProps> = ({ type, className, onClick, disabled = false, children }) => {
+	const getButtonStyle = () => {
+		switch (type) {
+			case 'basic':
+				return 'bg-orange-primary text-white hover:bg-orange-primary-hover border-orange-primary hover:border-orange-primary-hover'
+			case  'light':
+				return 'bg-transparent border border-white text-white hover:bg-white hover:text-dark-blue'
+		}
+	}
+
+	return (
+		<div
+			onClick={onClick}
+			className={c(getButtonStyle(), 'flex items-center justify-center transform-gpu transition cursor-pointer border', className, disabled && '!bg-gray-400 !border-gray-400 !text-gray-100 pointer-events-none')}
+		>
+			{children}
+		</div>
+	)
+}
+
+export default Button2
