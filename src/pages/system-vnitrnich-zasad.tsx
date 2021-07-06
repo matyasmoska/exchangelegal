9b08db5eb729f56c0c535ee5e584ca46fd31c39a
AMLsolutionsCZ <78909867+AMLsolutionsCZ@users.noreply.github.com>
2021-07-06 10:26:10 +0200

    Update system-vnitrnich-zasad.tsx

--- src/pages/system-vnitrnich-zasad.tsx
+++ src/pages/system-vnitrnich-zasad.tsx
@@ -66,7 +66,7 @@ const ObligationsPage = () => {
 						<BottomPartMdx />
 					</section>
 					<div className={c('flex')}>
-						<Link href="/potrebuji-aml">
+						<Link href="/nase-sluzby">
 							<Button type="basic" className="px-14 py-2.5">
 								{ pageData.buttonText }
 							</Button>
