6416229fc83e5dee03de2f9d5330280ced42dbc3
AMLsolutionsCZ <78909867+AMLsolutionsCZ@users.noreply.github.com>
2021-09-25 10:02:59 +0200

    Update co-je-to-aml.tsx

--- src/pages/co-je-to-aml.tsx
+++ src/pages/co-je-to-aml.tsx
@@ -12,7 +12,7 @@ export default function WhatIsAMLPage () {
 	return (
 		<DefaultLayout>
 			<SEO
-				title="AML solutions | Co je to AML?"
+				title="Co je to AML/CFT? – AML solutions"
 				description="Systémová řešení pro Vaše AML povinnosti na míru. Jsme předními odborníky v oblasti AML compliance"
 				keywords="AML, AML solutions, AML compliance, AML povinnosti, AML systém vnitřních zásad, AML školení, AML hodnocení rizik, AML dotazník, AML zákon, AML směrnice"
 			/>
