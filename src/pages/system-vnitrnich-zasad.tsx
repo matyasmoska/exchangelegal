d6813a3bb1f6b2ddc84df437a23065b4ec059f3a
AMLsolutionsCZ <78909867+AMLsolutionsCZ@users.noreply.github.com>
2021-10-24 17:30:35 +0200

    Update system-vnitrnich-zasad.tsx

--- src/pages/system-vnitrnich-zasad.tsx
+++ src/pages/system-vnitrnich-zasad.tsx
@@ -27,7 +27,7 @@ const ObligationsPage = () => {
     return (
         <DefaultLayout>
 			<SEO
-				title="Váš systém vnitřních zásad dle požadavků AML zákona 2021 – AML solutions"
+				title="Váš systém vnitřních zásad – připravíme dle požadavků AML zákona 2021 | AML solutions"
 				description="✅ Kvalitní systém vnitřních zásad připravíme na míru Vašemu podnikání. Jsme předními odborníky v oblasti AML compliance ⭐"
 				keywords="AML, AML solutions, AML compliance, AML povinnosti, AML systém vnitřních zásad, AML školení, AML hodnocení rizik, AML dotazník, AML zákon, AML směrnice"
 			/>
