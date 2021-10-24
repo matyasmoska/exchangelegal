e11d839bd6926da2fad3d9075cba8052b7f47e4f
AMLsolutionsCZ <78909867+AMLsolutionsCZ@users.noreply.github.com>
2021-10-24 17:40:15 +0200

    Update kontakty.tsx

--- src/pages/kontakty.tsx
+++ src/pages/kontakty.tsx
@@ -13,7 +13,7 @@ export default function Contact () {
 		<DefaultLayout>
 			<SEO
 				title="Kontakty – AML solutions"
-				description="Systémová řešení pro Vaše AML povinnosti na míru. Jsme předními odborníky v oblasti AML compliance"
+				description="✅ Jsme předními odborníky v oblasti AML compliance ⭐ Máme unikátní zkušenosti a know-how v AML/CFT"
 				keywords="AML, AML solutions, AML compliance, AML povinnosti, AML systém vnitřních zásad, AML školení, AML hodnocení rizik, AML dotazník, AML zákon, AML směrnice"
 			/>
 			<div
