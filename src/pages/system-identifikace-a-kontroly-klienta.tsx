98be1f5475f702f106399e7050b9919e59acc628
AMLsolutionsCZ <78909867+AMLsolutionsCZ@users.noreply.github.com>
2022-01-12 12:24:40 +0100

    Update system-identifikace-a-kontroly-klienta.tsx

--- src/pages/system-identifikace-a-kontroly-klienta.tsx
+++ src/pages/system-identifikace-a-kontroly-klienta.tsx
@@ -28,7 +28,7 @@ const ObligationsPage = () => {
         <DefaultLayout>
 			<SEO
 				title="AML dotazník – nezbytný pro AML/KYC – připravíme na míru | AML solutions"
-				description="✅ Kvalitní AML dotazník a systém vnitřních zásad dle požadavků AML zákona 2021. Jsme předními odborníky v oblasti AML compliance ⭐"
+				description="✅ Kvalitní AML dotazník a systém vnitřních zásad dle požadavků AML zákona 2022. Jsme předními odborníky v oblasti AML compliance ⭐"
 				keywords="AML, AML solutions, AML compliance, AML povinnosti, AML systém vnitřních zásad, AML školení, AML hodnocení rizik, AML dotazník, AML zákon, AML směrnice"
 			/>
 			<div className={c('relative items-center')}>
