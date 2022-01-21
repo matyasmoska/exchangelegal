2ef364a2f568e2a8300a653bc0d0cb7407530d57
AMLsolutionsCZ <78909867+AMLsolutionsCZ@users.noreply.github.com>
2022-01-21 17:34:01 +0100

    Update DefaultLayout1.tsx

--- src/layouts/DefaultLayout1.tsx
+++ src/layouts/DefaultLayout1.tsx
@@ -1,13 +1,10 @@
 import Head from 'next/head'
 import React, { FC, useEffect } from 'react'
-import Footer from '../components/Layout/Footer'
-import Navigation from '../components/Layout/Navigation'
-import SEO from '../components/Layout/SEO'
-import CookieBar from '../components/Layout/CookieBar'
-import { AnimatePresence } from 'framer-motion'
-import { useCookies } from 'react-cookie'
 
-const DefaultLayout: FC = ({ children }) => {
+import { AnimatePresence } from 'framer-motion'
+
+
+const DefaultLayout1: FC = ({ children }) => {
 	const [cookies, setCookie, removeCookie] = useCookies(['cookie-consent'])
 
 	useEffect(() => { console.log(cookies['cookie-consent']) }, [ cookies ])
@@ -40,4 +37,4 @@ const DefaultLayout: FC = ({ children }) => {
 	)
 }
 
-export default DefaultLayout
+export default DefaultLayout1
