03635029975eaf6f178edb2b2a1027beeb9b84dd
AMLsolutionsCZ <78909867+AMLsolutionsCZ@users.noreply.github.com>
2022-01-21 17:32:48 +0100

    Create DefaultLayout1.tsx

---
+++ src/layouts/DefaultLayout1.tsx
@@ -0,0 +1,43 @@
+import Head from 'next/head'
+import React, { FC, useEffect } from 'react'
+import Footer from '../components/Layout/Footer'
+import Navigation from '../components/Layout/Navigation'
+import SEO from '../components/Layout/SEO'
+import CookieBar from '../components/Layout/CookieBar'
+import { AnimatePresence } from 'framer-motion'
+import { useCookies } from 'react-cookie'
+
+const DefaultLayout: FC = ({ children }) => {
+	const [cookies, setCookie, removeCookie] = useCookies(['cookie-consent'])
+
+	useEffect(() => { console.log(cookies['cookie-consent']) }, [ cookies ])
+
+	return (
+		<div className="text-dark-blue">
+			<Head>		
+				<title>AML solutions</title>
+				<link rel="icon" href="/favicon.ico" />
+				<script async src="https://www.googletagmanager.com/gtag/js?id=UA-198544478-1" />
+
+				{ process.env.NODE_ENV === 'production' && <script dangerouslySetInnerHTML={{ __html: `
+                    window.dataLayer = window.dataLayer || [];
+					function gtag(){dataLayer.push(arguments);}
+					gtag('js', new Date());
+				  
+					gtag('config', 'UA-198544478-1');
+                `} }/>}
+				
+				<script type="application/ld+json" dangerouslySetInnerHTML={{
+					__html: `{ "@context": "https://schema.org/", "@type": "CreativeWorkSeries", "name": "Post title", "aggregateRating": { "@type": "AggregateRating", "ratingValue": "4.9", "bestRating": "5", "ratingCount": "42" } }`
+				}}/>
+				
+			</Head>
+			<Navigation />
+			{children}
+			<Footer />
+			<AnimatePresence>{!cookies['cookie-consent'] && <CookieBar />}</AnimatePresence>
+		</div>
+	)
+}
+
+export default DefaultLayout
