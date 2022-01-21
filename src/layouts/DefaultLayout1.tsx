6a87774db8aa698e9fc22c089e542b647035fec9
AMLsolutionsCZ <78909867+AMLsolutionsCZ@users.noreply.github.com>
2022-01-21 17:36:35 +0100

    Update DefaultLayout1.tsx

--- src/layouts/DefaultLayout1.tsx
+++ src/layouts/DefaultLayout1.tsx
@@ -1,11 +1,16 @@
 import Head from 'next/head'
 import React, { FC, useEffect } from 'react'
-
+import Footer from '../components/Layout/Footer'
+import Navigation from '../components/Layout/Navigation'
+import SEO from '../components/Layout/SEO'
+import CookieBar from '../components/Layout/CookieBar'
 import { AnimatePresence } from 'framer-motion'
+import { useCookies } from 'react-cookie'
 
+const DefaultLayout: FC = ({ children }) => {
+	const [cookies, setCookie, removeCookie] = useCookies(['cookie-consent'])
 
-const DefaultLayout1: FC = ({ children }) => {
-
+	useEffect(() => { console.log(cookies['cookie-consent']) }, [ cookies ])
 
 	return (
 		<div className="text-dark-blue">
@@ -27,12 +32,12 @@ const DefaultLayout1: FC = ({ children }) => {
 				}}/>
 				
 			</Head>
-			
+			<Navigation />
 			{children}
-			
+			<Footer />
 			<AnimatePresence>{!cookies['cookie-consent'] && <CookieBar />}</AnimatePresence>
 		</div>
 	)
 }
 
-export default DefaultLayout1
+export default DefaultLayout
