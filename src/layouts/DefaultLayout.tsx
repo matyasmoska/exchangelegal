312b6e9678ae2c5c4c666b4f0874e4ffd145b5b2
matyasmoska <99955136+matyasmoska@users.noreply.github.com>
2022-07-06 18:11:20 +0200

    Update DefaultLayout.tsx

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -25,7 +25,7 @@ const DefaultLayout: FC = ({ children }) => {
 	return (
 		<div className="text-dark-blue">
 			<Head>		
-				<title>AML solutions</title>
+				<title>15zisif.cz</title>
 				<link rel="icon" href="/favicon.ico" />
 				{ process.env.NODE_ENV === 'production' && <script dangerouslySetInnerHTML={{ __html: GA_SCRIPT_DEFAULT }} />}
 				<script async src="https://www.googletagmanager.com/gtag/js?id=UA-198544478-1" />
@@ -45,7 +45,7 @@ const DefaultLayout: FC = ({ children }) => {
                 `} }/>}
 				
 				<script type="application/ld+json" dangerouslySetInnerHTML={{
-					__html: `{ "@context": "https://schema.org/", "@type": "CreativeWorkSeries", "name": "Post title", "aggregateRating": { "@type": "AggregateRating", "ratingValue": "4.9", "bestRating": "5", "ratingCount": "42" } }`
+					__html: `{ "@context": "https://schema.org/", "@type": "CreativeWorkSeries", "name": "Post title", "aggregateRating": { "@type": "AggregateRating", "ratingValue": "4.9", "bestRating": "5", "ratingCount": "14" } }`
 				}}/>
 				
 			</Head>
