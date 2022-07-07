8c5562b8b913cfea9dbc88ef89fd0b270434c787
matyasmoska <99955136+matyasmoska@users.noreply.github.com>
2022-07-07 15:32:05 +0200

    Update PEPSection.tsx

--- src/components/Pages/index/MainCarouselSections/PEPSection.tsx
+++ src/components/Pages/index/MainCarouselSections/PEPSection.tsx
@@ -8,55 +8,53 @@ const PEPSection = () => {
 	const { isMd } = useMediaQueries()
 
 	return (
-		<div
+				<div
 			className={c(
-				'relative w-full bg-cover px-36 py-36 pt-24 h-[602px] bg-white',
-				'3xl:px-28 3xl:pr-32',
-				'md:px-5 md:pt-36 md:h-[704px]'
+				'relative w-full bg-cover px-36 py-36 pt-24 h-[602px]',
+				'3xl:px-28',
+				'md:px-5 md:pt-28 md:h-[724px]'
 			)}
 		>
-			<img
-				src={'/images/third-background.jpg'}
+			<Image
+				layout="fill"
+				objectFit="cover"
+				priority
+				placeholder="blur"
+				src={thumbnail}
 				alt="Background Image"
-				className={c('absolute max-w-xl pt-24 top-10 right-52', '3xl:right-24', '2xl:right-6 2xl:max-w-lg 2xl:pt-28')}
+				className="absolute top-0 left-0 z-0"
 			/>
 			<div
 				className={c(
 					'absolute top-0 left-0 z-10 w-full h-full from-dark-blue via-[#021C62A6] bg-gradient-to-r to-transparent',
-					'md:bg-dark-blue md:opacity-100'
+					'md:to-dark-blue md:opacity-80'
 				)}
-				style={{
-					background: !isMd
-						? 'transparent linear-gradient(90deg, #223a7b 0%, #021C62D8 25%, #021C6279 42%, #021C620A 62%, #021C6200 100%) 0% 0% no-repeat padding-box'
-						: ''
-				}}
 			/>
-			<div
-				className={c(
-					'relative z-10 max-w-3xl space-y-12 text-white',
-					'2xl:max-w-xl',
-					'md:max-w-none md:text-center md:flex md:flex-col md:items-center'
-				)}
-			>
+
+			<div className={c('relative z-10 max-w-3xl space-y-20 text-white', 'md:max-w-none md:text-center')}>
 				<div className="flex flex-col space-y-8">
 					<h1
-						dangerouslySetInnerHTML={{ __html: pageData.pepCheckSection.title }}
-						className={c('text-5xl font-bold leading-tight', 'md:text-3xl')}
+						dangerouslySetInnerHTML={{ __html: pageData.secondarySection.title }}
+						className={c('text-6xl font-bold leading-tight', 'md:text-4xl')}
 					/>
-					<p className="text-lg leading-relaxed text-justify">{pageData.pepCheckSection.subtitle}</p>
+					<p className="text-lg leading-relaxed text-justify">{pageData.secondarySection.subtitle}</p>
 				</div>
 				<div
 					className={c(
 						'grid gap-6 items-center w-9/12 grid-cols-2',
-						'2xl:w-full',
 						'md:block md:space-y-6 md:w-full md:space-x-0'
 					)}
 				>
-					<a href="https://www.pepcheck.cz" target="_blank" rel="noreferrer">
-						<Button type="basic" className="px-6 py-2">
-							{pageData.pepCheckSection.buttonText}
+					<a href="https://www.amlsolutions.cz/overovani-mezinarodnich-sankci" target="_blank" rel="noreferrer">
+						<Button type="basic" className="px-3 py-2">
+							{pageData.secondarySection.firstButtonText}
 						</Button>
 					</a>
+					<Link href="/seznam-vysoce-rizikovych-zemi">
+						<Button type="light" className="px-10 py-2">
+							{pageData.secondarySection.secondButtonText}
+						</Button>
+					</Link>
 				</div>
 			</div>
 		</div>
