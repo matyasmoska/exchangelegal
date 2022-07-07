795fd3fffd923d30ce6be86f915d2c45bdcdf778
matyasmoska <99955136+matyasmoska@users.noreply.github.com>
2022-07-07 13:24:44 +0200

    Update PartnersSection.tsx

--- src/components/Pages/index/PartnersSection.tsx
+++ src/components/Pages/index/PartnersSection.tsx
@@ -10,22 +10,19 @@ const PartnersSection: FC = () => {
 			<div className={c('flex justify-center space-x-32', 'md:flex-col md:items-center md:space-x-0 md:space-y-16')}>
 				<Fade damping={0.5} duration={500} cascade triggerOnce>
 					
-					<a href="https://www.realitnikomora.cz/" target="_blank" rel="noreferrer">
-						<img alt="realitni-komora-logo" className="h-14" src={'/images/realitni-komora_logo.jpg'} />
+					<a href="https://www.amlsolutions.cz/" target="_blank" rel="noreferrer">
+						<img alt="aml-solutions-logo" className="h-14" src={'/images/aml-solutions.png'} />
 					</a>
 					
-					<a href="http://www.ondato.com/" target="_blank" rel="noreferrer">
-						<img alt="ondato-logo" className="h-14" src={'/images/ondato.png'} />
+					<a href="http://www.moskamurad.legal" target="_blank" rel="noreferrer">
+						<img alt="moskamurad-logo" className="h-14" src={'/images/moska-murad.png'} />
 					</a>
 					
-					<a href="https://www.pozemkovekompenzace.cz/" target="_blank" rel="noreferrer">
-						<img alt="pkf-logo" className="h-14" src={'/images/PKF logo.png'} />
+					<a href="https://www.pepcheck.cz/" target="_blank" rel="noreferrer">
+						<img alt="pep-check-logo" className="h-14" src={'/images/pep-check.png'} />
 					</a>
-					<a href="https://billsystems.co" target="_blank" rel="noreferrer">
-						<img alt="bill_logo" className="h-14" src={'/images/bill.png'} />
-					</a>
-					<a href="https://www.realitniakademie.eu/" target="_blank" rel="noreferrer">
-						<img alt="realitni_akademie_logo" className="h-14" src={'/images/realitni-akademie_logo.jpg'} />
+					<a href="https://www.safe-whistlers.cz" target="_blank" rel="noreferrer">
+						<img alt="safe-whistlers-logo" className="h-14" src={'/images/safe-whistlers.png'} />
 					</a>
 					
 				</Fade>
