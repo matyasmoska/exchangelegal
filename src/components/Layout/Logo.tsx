d9060ec6557f29809f8bb8d615479c016aee868a
matyasmoska <99955136+matyasmoska@users.noreply.github.com>
2022-07-07 15:00:51 +0200

    Update Logo.tsx

--- src/components/Layout/Logo.tsx
+++ src/components/Layout/Logo.tsx
@@ -17,7 +17,7 @@ const Logo = () => {
 export const LogoWhite = () => {
 	const { isMd } = useMediaQueries()
 
-	return <Image alt="15zisif.cz" src="/images/logo-white.svg" height={isMd ? 35 : 160} width={isMd ? 200 : 370} />
+	return <Image alt="15zisif.cz" src="/images/logo-white.svg" height={isMd ? 35 : 40} width={isMd ? 200 : 240} />
 }
 
 export default Logo
