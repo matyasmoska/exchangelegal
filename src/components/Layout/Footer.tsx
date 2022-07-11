3a2bb6d05c8310b46e0c6c60ac48f1b1cb589f2a
matyasmoska <99955136+matyasmoska@users.noreply.github.com>
2022-07-11 15:11:07 +0200

    Update Footer.tsx

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -99,7 +99,7 @@ const Footer = () => {
 							<a href={`mailto:${contactPageData.contact.email}`} className="hover:underline">
 								{contactPageData.contact.email}
 							</a>
-							<a className="hover:underline" href="/docs/obchodni_podminky_AML_solutions.pdf" target="_blank">{pageData.tos}</a>
+							<a className="hover:underline" href="/pravni-informace-a-podminky-uziti" target="_blank">{pageData.tos}</a>
 							<Link href="/zasady-zpracovani-osobnich-udaju">
 								<a className="hover:underline">{pageData.privacypolicy}</a>
 							</Link>
