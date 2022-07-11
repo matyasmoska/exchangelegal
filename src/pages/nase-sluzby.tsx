7dab2ca062281a86f9ebd648b700b885e4b255de
matyasmoska <99955136+matyasmoska@users.noreply.github.com>
2022-07-11 13:31:14 +0200

    Update nase-sluzby.tsx

--- src/pages/nase-sluzby.tsx
+++ src/pages/nase-sluzby.tsx
@@ -27,8 +27,8 @@ export default function Services () {
 		<DefaultLayout>
 			<SEO
 				title="Naše služby | 15zisif.cz"
-				description="Jsme předními odborníky v oblasti AML compliance, poskytujeme komplexní řešení a služby ✅ Úvodní konzultace zdarma ⭐"
-				keywords="AML, AML solutions, AML compliance, AML povinnosti, AML systém vnitřních zásad, AML školení, AML hodnocení rizik, AML dotazník, AML zákon, AML směrnice"
+				description="✅ Jsme odborníky v oblasti zakládání fondů ⭐ Máme unikátní zkušenosti a know-how v oblasti minifondů dle § 15 ZISIF"
+				keywords="minifond, alternativní fond, § 15 ZISIF, 15zisif"
 			/>
 			<div className={c('py-16 space-y-12 text-center px-36', 'md:px-4 md:py-8 md:relative')}>
 				<h1 className="text-5xl font-bold leading-snug">Naše služby</h1>
