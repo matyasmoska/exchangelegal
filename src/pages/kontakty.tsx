59fbcd6a13e5f1c055e81731781f429e68b13ce0
matyasmoska <99955136+matyasmoska@users.noreply.github.com>
2022-07-11 13:30:55 +0200

    Update kontakty.tsx

--- src/pages/kontakty.tsx
+++ src/pages/kontakty.tsx
@@ -13,8 +13,8 @@ export default function Contact () {
 		<DefaultLayout>
 			<SEO
 				title="Kontakty – 15zisif.cz"
-				description="✅ Jsme předními odborníky v oblasti AML compliance ⭐ Máme unikátní zkušenosti a know-how v AML/CFT"
-				keywords="AML, AML solutions, AML compliance, AML povinnosti, AML systém vnitřních zásad, AML školení, AML hodnocení rizik, AML dotazník, AML zákon, AML směrnice"
+				description="✅ Jsme odborníky v oblasti zakládání fondů ⭐ Máme unikátní zkušenosti a know-how v oblasti minifondů dle § 15 ZISIF"
+				keywords="minifond, alternativní fond, § 15 ZISIF, 15zisif"
 			/>
 			<div
 				className={c(
