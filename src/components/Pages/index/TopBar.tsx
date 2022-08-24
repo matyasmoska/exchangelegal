8cf6f44b3df82b4206df4d6da9f9bee4da9611e6
Josef Janoušek <pauto@centrum.cz>
2022-08-24 22:44:49 +0200

    TopBar - change of icons and font

--- src/components/Pages/index/TopBar.tsx
+++ src/components/Pages/index/TopBar.tsx
@@ -1,6 +1,5 @@
 import { AnimateSharedLayout, motion as m } from 'framer-motion'
 import React, { FC } from 'react'
-import { PhoneIcon, MessageIcon, TwitterIcon, LinkedInIcon } from '../../Layout/Icons'
 import contactPageData from '../../../data/pages/index.json'
 
 /*
@@ -11,26 +10,26 @@ import contactPageData from '../../../data/pages/index.json'
 const TopBar: FC = () => {
 	return (
 		<AnimateSharedLayout>
-		    <m.div layout className="bg-dark-blue">
+		    <m.div layout className="bg-dark-blue font-header font-semibold">
                 <div className="flex flex-wrap gap-x-4 gap-y-2 py-3 px-36 2xl:px-24 xl:px-16 md:px-8 xs:px-4 bg-white text-dark-blue bg-opacity-70">
                     <div className="flex gap-2">
-                        <MessageIcon className="w-6 h-6" />
+                        <img src="/images/mail-icon.svg" className="w-6 h-6" />
                         <a href={`mailto:${contactPageData.contact.email}`} className="hover:underline">
                             {contactPageData.contact.email}
                         </a>
                     </div>
                     <div className="flex gap-2">
-                        <PhoneIcon className="w-6 h-6" />
+                        <img src="/images/phone-icon.svg" className="w-6 h-6" />
                         <a href={`tel:${contactPageData.contact.phoneNumber}`} className="hover:underline">
                             {contactPageData.contact.phoneNumber}
                         </a>
                     </div>
-                    <div className="flex gap-4 ml-auto">
-                        <a href={contactPageData.contact.links.twitter} className="text-white bg-dark-blue rounded-full" target="_blank" rel="noopener" aria-label="Twitter Link">
-                            <TwitterIcon className="fill-current w-6 h-6" />
+                    <div className="flex gap-4">
+                        <a href={contactPageData.contact.links.twitter} target="_blank" rel="noopener" aria-label="Twitter Link">
+                            <img src="/images/twitter-icon.svg" className="w-6 h-6" />
                         </a>
-                        <a href={contactPageData.contact.links.linkedIn} className="text-white bg-dark-blue rounded-full" target="_blank" rel="noopener" aria-label="Linked In Link">
-                            <LinkedInIcon className="fill-current w-6 h-6" />
+                        <a href={contactPageData.contact.links.linkedIn} target="_blank" rel="noopener" aria-label="Linked In Link">
+                            <img src="/images/linkedin-icon.svg" className="w-6 h-6" />
                         </a>
                     </div>
     			</div>
