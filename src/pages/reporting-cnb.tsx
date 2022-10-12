cf947a6b10a1d7e7dad3a9d682fb0a111edf0627
matyasmoska <99955136+matyasmoska@users.noreply.github.com>
2022-10-12 16:13:54 +0200

    Update reporting-cnb.tsx

--- src/pages/reporting-cnb.tsx
+++ src/pages/reporting-cnb.tsx
@@ -52,7 +52,7 @@ const ObligationsPage = () => {
 				>
 
 					<section className={c('py-8 pb-12 space-y-4 max-w-[802px] leading-relaxed prose', 'md:px-6 md:py-6')}>
-						<BottomPartMdx />
+						<topPartMdx />
 					</section>
 					<div className={c('flex')}>
 
