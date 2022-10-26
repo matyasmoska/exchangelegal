f15f219316a8ed6a39ef6aa7f752f288b1526e60
matyasmoska <99955136+matyasmoska@users.noreply.github.com>
2022-10-26 16:30:35 +0200

    Update reporting-cnb.tsx

--- src/pages/reporting-cnb.tsx
+++ src/pages/reporting-cnb.tsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ParagraphOrMultiple from "../components/Layout/ParagraphOrMultiple";
 import DefaultLayout from "../layouts/DefaultLayout";
 import SEO from "../components/Layout/SEO";
 import { c } from "../services/misc";
 import Image from 'next/image'
 import pageData from '../data/pages/reporting-cnb/reporting-cnb.json'
+
+import servicesData from '../data/pages/services.json'
+import useServicesForm from "../components/Pages/services/hooks/useServicesForm";
+import OrderButton from "../components/Pages/services/OrderButton";
+import ServicesForm from "../components/Pages/services/ServícesForm";
+import { useVisible } from "react-hooks-visible";
+
 import Button from "../components/Layout/Button";
 import Link from "next/link";
 // @ts-ignore
@@ -13,6 +20,15 @@ import TopPartMdx from "../data/pages/reporting-cnb/topPart.mdx"
 import BottomPartMdx from "../data/pages/reporting-cnb/bottomPart.mdx"
 
 const ObligationsPage = () => {
+	
+	const [targetRef, visible] = useVisible()
+
+	const servicesForm = useServicesForm()
+
+	useEffect(() => {
+		servicesForm.setFieldValue('checked', servicesData.services.filter(({ id }) => id === 'reporting-cnb'))
+	}, [])
+	
     return (
         <DefaultLayout>
 			<SEO
