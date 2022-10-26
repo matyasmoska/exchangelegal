b1ace5ed3099a3a566115a10e4628f921ee11975
matyasmoska <99955136+matyasmoska@users.noreply.github.com>
2022-10-26 17:38:32 +0200

    Update investicni-smlouva.tsx

--- src/pages/investicni-smlouva.tsx
+++ src/pages/investicni-smlouva.tsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ParagraphOrMultiple from "../components/Layout/ParagraphOrMultiple";
 import DefaultLayout from "../layouts/DefaultLayout";
 import SEO from "../components/Layout/SEO";
 import { c } from "../services/misc";
 import Image from 'next/image'
 import pageData from '../data/pages/investicni-smlouva/investicni-smlouva.json'
+
+import servicesData from '../data/pages/services.json'
+import useServicesForm from "../components/Pages/services/hooks/useServicesForm";
+import OrderButton from "../components/Pages/services/OrderButton";
+import ServicesForm from "../components/Pages/services/ServícesForm";
+import { useVisible } from "react-hooks-visible";
+
 import Button from "../components/Layout/Button";
 import Link from "next/link";
 // @ts-ignore
@@ -13,6 +20,15 @@ import TopPartMdx from "../data/pages/investicni-smlouva/topPart.mdx"
 import BottomPartMdx from "../data/pages/investicni-smlouva/bottomPart.mdx"
 
 const ObligationsPage = () => {
+	
+			const [targetRef, visible] = useVisible()
+
+	const servicesForm = useServicesForm()
+
+	useEffect(() => {
+		servicesForm.setFieldValue('checked', servicesData.services.filter(({ id }) => id === 'investicni-smlouva'))
+	}, [])
+	
     return (
         <DefaultLayout>
 			<SEO
@@ -58,7 +74,9 @@ const ObligationsPage = () => {
 
 					</div>
 				</div>
+									<OrderButton show={!visible} text={pageData.buttonText} />
 			</div>
+		    <ServicesForm visibleRef={targetRef} form={servicesForm} />
 		</DefaultLayout>
     );
 }
