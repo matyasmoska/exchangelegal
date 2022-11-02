beec5ee85d4d9925eba7aa147b9b0edcfa1a246b
matyasmoska <99955136+matyasmoska@users.noreply.github.com>
2022-11-02 10:17:22 +0100

    Update next-sitemap.js

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://www.amlsolutions.cz',
+    siteUrl: process.env.SITE_URL || 'https://www.15zisif.cz',
     generateRobotsTxt: true,
     priority: 0.7,
 }
