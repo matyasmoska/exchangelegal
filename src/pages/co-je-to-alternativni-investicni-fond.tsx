f0637c89904c425a4613217a233f1f3c5fb23f15
matyasmoska <99955136+matyasmoska@users.noreply.github.com>
2023-06-01 13:05:09 +0200

    Update co-je-to-alternativni-investicni-fond.tsx

--- src/pages/co-je-to-alternativni-investicni-fond.tsx
+++ src/pages/co-je-to-alternativni-investicni-fond.tsx
@@ -16,7 +16,7 @@ const ObligationsPage = () => {
     return (
         <DefaultLayout>
 			<SEO
-				title="Alternativní investiční fond – vše, co potřebujete vědět – 15zisif.cz"
+				title="15 ZISIF – alternativní investiční fond – vše, co potřebujete vědět – 15zisif.cz"
 				description="✅ Jsme odborníky v oblasti zakládání fondů ⭐ Máme unikátní zkušenosti a know-how v oblasti alternativních fondů dle § 15 ZISIF"
 				keywords="alternativní investiční fond, minifond, alternativní fond, § 15 ZISIF, 15zisif"
 			/>
