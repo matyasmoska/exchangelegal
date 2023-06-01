a7cab9a8e70afaa94fc81f0baa41079854b98337
matyasmoska <99955136+matyasmoska@users.noreply.github.com>
2023-06-01 13:10:52 +0200

    Update kontakty.tsx

--- src/pages/kontakty.tsx
+++ src/pages/kontakty.tsx
@@ -12,9 +12,9 @@ export default function Contact () {
 	return (
 		<DefaultLayout>
 			<SEO
-				title="Kontakty – 15zisif.cz"
-				description="✅ Jsme odborníky v oblasti zakládání fondů ⭐ Máme unikátní zkušenosti a know-how v oblasti minifondů dle § 15 ZISIF"
-				keywords="minifond, alternativní fond, § 15 ZISIF, 15zisif"
+				title="15 ZISIF – kontakty – 15zisif.cz"
+description="✅ Jsme odborníky v oblasti zakládání fondů ⭐ Máme unikátní zkušenosti a know-how v oblasti alternativních fondů dle § 15 ZISIF"
+keywords="alternativní investiční fond, minifond, alternativní fond, § 15 ZISIF, 15zisif"
 			/>
 			<div
 				className={c(
