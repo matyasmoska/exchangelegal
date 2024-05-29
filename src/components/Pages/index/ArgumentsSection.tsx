ca2e1474a2c91cdae485d4c5de149e9af9526b7c
matyasmoska <99955136+matyasmoska@users.noreply.github.com>
2024-05-29 10:50:18 +0200

    Update ArgumentsSection.tsx

--- src/components/Pages/index/ArgumentsSection.tsx
+++ src/components/Pages/index/ArgumentsSection.tsx
@@ -87,28 +87,7 @@ const ArgumentsSection: FC = () => {
 			</Fade>
 		</div>
 
-		<div className={c('font-header w-full space-y-16 mt-24 px-36', '3xl:px-28', '2xl:px-20', 'md:px-8')}>
-			<Fade direction={'up'} triggerOnce>
-				<h2 dangerouslySetInnerHTML={{ __html: data.argumentsTitle }} className="text-4xl font-bold text-center" />
-			</Fade>
-			<div className={c('grid grid-cols-3 gap-x-16 gap-y-12', 'lg:grid-cols-2 lg:gap-12', 'md:grid-cols-1 md:gap-8')}>
-				<Fade damping={0.5} duration={500} cascade triggerOnce>
-					{data.arguments.map(({ icon, text, title }) => (
-						<div
-							key={title}
-							className={c(
-								'h-full min-h-argument p-5 space-y-4 text-center shadow-tilearg border borderGradient',
-								'md:max-w-md md:mx-auto'
-							)}
-						>
-							<img className="w-12 h-12 mx-auto" src={icon} />
-							<h3 className="text-xl font-bold py-0.5">{title}</h3>
-							<p className="max-w-xs mx-auto">{text}</p>
-						</div>
-					))}
-				</Fade>
-			</div>
-		</div>
+
 
 		<div className={c('font-header w-full space-y-8 mt-24 px-36', '3xl:px-28', '2xl:px-20', 'md:px-8')}>
 			<Fade direction={'up'} triggerOnce>
