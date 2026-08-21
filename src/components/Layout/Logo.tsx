daa650b20367592fba779e03e24be82c21bcbe42
matyasmoska <matyasmoska@gmail.com>
2026-08-21 13:59:44 +0200

    Verze 17

--- src/components/Layout/Logo.tsx
+++ src/components/Layout/Logo.tsx
@@ -6,7 +6,7 @@ const Logo = () => {
 	return (
 		<Link href="/">
 			<a className="z-50">
-				<img alt="pravoprosmenarny.cz" src="/images/logo.svg" className="h-16 w-auto md:h-12" height={64} width={186} />
+				<img alt="pravoprosmenarny.cz" src="/images/logo.svg" className="h-14 w-auto md:h-10" height={56} width={158} />
 			</a>
 		</Link>
 	)
@@ -15,7 +15,7 @@ const Logo = () => {
 export const LogoWhite = () => {
 	const { isMd } = useMediaQueries()
 
-	return <Image alt="pravoprosmenarny.cz" src="/images/logo-white.svg" height={isMd ? 55 : 103} width={isMd ? 160 : 300} />
+	return <Image alt="pravoprosmenarny.cz" src="/images/logo-white.svg" height={isMd ? 64 : 106} width={isMd ? 180 : 300} />
 }
 
 export default Logo
